fd94b25133d9de6da7dd3e3b64b5946e9eb5acd9
Jamie Legg <legg.jamie@gmail.com>
2022-01-18 16:30:04 +0000

    icon modal design update

--- app/core/components/AccountsModal.tsx
+++ app/core/components/AccountsModal.tsx
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useEffect, useState } from "react"
 import { Dialog, Transition } from "@headlessui/react"
-import { CheckIcon } from "@heroicons/react/outline"
+import { CheckIcon, XIcon } from "@heroicons/react/outline"
 import Button from "./Button"
 import Typist from "react-typist"
 import getAccountData from "../../api/getAccountData"
@@ -120,9 +120,41 @@ export default function AccountsModal({ isOpen, onClose }) {
                       </div>
                     ) : attemptedSearch ? null : null}
                   </div>
-                  <div className="bg-indigo-300 w-full rounded-2xl py-3 px-6">
-                    <div></div>
-                    <CheckIcon className="h-4 w-4" />
+                  <div className="bg-indigo-300 grid grid-cols-3 text-left text-indigo-700 uppercase font-bold w-full rounded-2xl py-3 px-6">
+                    <div>SYN#1437</div>
+                    <div className="flex place-content-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                        />
+                      </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                        />
+                      </svg>
+                    </div>
+                    <div className="flex flex-row-reverse">
+                      <XIcon className="w-6 h-6 text-red-600" />
+                    </div>
                   </div>
                 </div>
               </div>
